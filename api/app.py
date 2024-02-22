7d96f348c1ec2dbab300444ea9dd56ad2d71175a
Giorgos Zachariadis <gzachos5@gmail.com>
2024-02-22 15:24:59 +0200

    change host

--- api/app.py
+++ api/app.py
@@ -59,9 +59,9 @@ def predict():
         )
     except TypeError as e:
         result = jsonify({"error": str(e)})
-        
+
     return result
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(debug=True)
